4f45aae451de17951e8099c0f53bd7c2dc4bf40f
Saif Ullah (Jailani Syed) <xl8.saif@gmail.com>
2026-09-22 23:07:33 +0500

    Configure Vercel serverless runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 // On GitHub Pages this becomes https://<user>.github.io/<repo>/
 const site = process.env.SITE_URL || 'http://localhost:4321';
 
 export default defineConfig({
   site,
+  output: 'server',
+  adapter: vercel(),
   integrations: [sitemap()],
 });
